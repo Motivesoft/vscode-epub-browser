4e369f193bf003c725ad7ebe5407d218f01c4feb
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-12-08 16:11:15 +0100

    Can now open an epub file and browse contents

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,53 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as epubfs from './epubHandler';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('Extension "vscode-epub-browser" is active');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-epub-browser" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-epub-browser.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from EPUB browser!');
-	});
+	const filesystemScheme = "epubfs";
 
-	context.subscriptions.push(disposable);
+	const fileSystem = new epubfs.ReadonlyFS();
+	context.subscriptions.push(vscode.workspace.registerFileSystemProvider(filesystemScheme, fileSystem, { isCaseSensitive: true, isReadonly: true }));
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-epub-browser.mount', async () => {
+		vscode.window.showInformationMessage('Mount EPUB file');
+
+		// Let the user choose a file. Don't constrain this to being something in the currently open folder
+		const options: vscode.OpenDialogOptions = {
+			canSelectMany: false,
+			canSelectFiles: true,
+			canSelectFolders: false,
+			title: 'Select epub file',
+			openLabel: 'Mount',
+			filters: {
+				'EPUB files': ['epub']
+			}
+		};
+
+		// Display the File dialog
+		const fileUri = await vscode.window.showOpenDialog(options);
+
+		// Proceed if a file was selected. 
+		// - Assume it is of the correct format (something readable as a zip file)
+		// - The file dialog is configured to only allow a single selection, not mulitple
+		if (fileUri && fileUri[0]) {
+			const _uri = fileUri[0];
+			vscode.window.showInformationMessage(`Mounting ${_uri}`);
+
+			const uri = vscode.Uri.parse(`${filesystemScheme}:/?${_uri}`);
+			if (vscode.workspace.getWorkspaceFolder(uri) === undefined) {
+			  const name = vscode.workspace.asRelativePath(_uri, true);
+			  const index = vscode.workspace.workspaceFolders?.length || 0;
+			  const workspaceFolder: vscode.WorkspaceFolder = { uri, name, index };
+			  vscode.workspace.updateWorkspaceFolders(index, 0, workspaceFolder);
+			}
+ 		}
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-epub-browser.unmount', () => {
+		vscode.window.showInformationMessage('Unmount EPUB file');
+	}));
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
